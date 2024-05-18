9cb1622c2460a1a5a034b17a7f54afa671ea07af
Amir Khan <amir.khan@salesforce.com>
2024-05-18 21:53:06 +0200

    Persistent Memory Method added
    New operation with persistent memory added.

--- src/main/java/org/mule/extension/langchain/internal/llm/LangchainLLMOperations.java
+++ src/main/java/org/mule/extension/langchain/internal/llm/LangchainLLMOperations.java
@@ -4,6 +4,7 @@ import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.load
 import static java.time.Duration.ofSeconds;
 import static org.mule.runtime.extension.api.annotation.param.MediaType.ANY;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,7 +35,25 @@ import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.store.memory.chat.ChatMemoryStore;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
 
+
+import java.util.List;
+import java.util.Map;
+
+import static dev.langchain4j.data.message.ChatMessageDeserializer.messagesFromJson;
+import static dev.langchain4j.data.message.ChatMessageSerializer.messagesToJson;
+import static org.mapdb.Serializer.INTEGER;
+import static org.mapdb.Serializer.STRING;
 /**
  * This class is a container for operations, every public method in this class will be taken as an extension operation.
  */
@@ -251,6 +270,90 @@ public class LangchainLLMOperations {
   
   
   
+  interface Assistant {
+
+      String chat(@MemoryId int memoryId, @UserMessage String userMessage);
+  }
+
+
+  /**
+   * Example of a simple operation that receives a string parameter and returns a new string message that will be set on the payload.
+   */
+  @MediaType(value = ANY, strict = false)
+  @Alias("Persistent-memory")  
+  public String chatWithPersistentMemory(String data, @Config LangchainLLMConfiguration configuration, @ParameterGroup(name= "Additional properties") LangchainLLMParameters LangchainParams) {
+      
+	  ChatLanguageModel model = null;
+		switch(configuration.getLlmType()) {
+		case "OPENAI_API_KEY": 
+			
+		      model = OpenAiChatModel.builder()
+		              .apiKey(configuration.getLlmApiKey())
+		              .modelName(LangchainParams.getModelName())
+		              .temperature(0.3)
+		              .timeout(ofSeconds(60))
+		              .logRequests(true)
+		              .logResponses(true)
+		              .build();
+		      break;
+		      
+		case "MISTRALAI_API_KEY": 
+			
+		      model = MistralAiChatModel.builder()
+		              .apiKey(configuration.getLlmApiKey())
+		              .modelName(LangchainParams.getModelName())
+		              .temperature(0.3)
+		              .timeout(ofSeconds(60))
+		              .logRequests(true)
+		              .logResponses(true)
+		              .build();
+		      break;
+		}
+	  
+
+		PersistentChatMemoryStore store = new PersistentChatMemoryStore();
+
+
+
+        ChatMemoryProvider chatMemoryProvider = memoryId -> MessageWindowChatMemory.builder()
+                .id(memoryId)
+                .maxMessages(10)
+                .chatMemoryStore(store)
+                .build();
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatLanguageModel(model)
+                .chatMemoryProvider(chatMemoryProvider)
+                .build();
+
+        return assistant.chat(1, data);
+
+  }
+
+  static class PersistentChatMemoryStore implements ChatMemoryStore {
+
+      private final DB db = DBMaker.fileDB("/Users/amir.khan/Documents/langchain4mule resources/multi-user-chat-memory.db").transactionEnable().fileLockDisable().make();
+      private final Map<Integer, String> map = db.hashMap("messages", INTEGER, STRING).createOrOpen();
+
+      @Override
+      public List<ChatMessage> getMessages(Object memoryId) {
+          String json = map.get((int) memoryId);
+          return messagesFromJson(json);
+      }
+
+      @Override
+      public void updateMessages(Object memoryId, List<ChatMessage> messages) {
+          String json = messagesToJson(messages);
+          map.put((int) memoryId, json);
+          db.commit();
+      }
+
+      @Override
+      public void deleteMessages(Object memoryId) {
+          map.remove((int) memoryId);
+          db.commit();
+      }
+  }  
   
   
   
