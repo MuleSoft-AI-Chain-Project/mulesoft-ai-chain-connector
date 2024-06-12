30e7312d7464f732d4b9b027316ae0b83311bdda
Veebz <vibhor.sharma@salesforce.com>
2024-06-12 11:24:34 +0100

    updated files

--- src/main/java/org/mule/extension/langchain/internal/embedding/models/LangchainEmbeddingModelsOperations.java
+++ src/main/java/org/mule/extension/langchain/internal/embedding/models/LangchainEmbeddingModelsOperations.java
@@ -1,29 +1,9 @@
 package org.mule.extension.langchain.internal.embedding.models;
 
 
-/**
- * This class is a container for operations, every public method in this class will be taken as an extension operation.
- */
+
 public class LangchainEmbeddingModelsOperations {
 
-	
-	
-
-//  /**
-//   * Example of an operation that uses the configuration and a connection instance to perform some action.
-//   */
-//  @MediaType(value = ANY, strict = false)
-//  @Alias("Apply-classification")
-//  public String predict(String prompt, String jsonString, @Config LangchainEmbeddingModelConfiguration configuration){
-//	  
-//      //System.out.println(categories); // [ORDER_STATUS]	    
-//
-//	  return "";
-//  }
-
-	
-	
-	
 	
 	
 	
--- src/main/java/org/mule/extension/langchain/internal/llm/LangchainLLMParameterModelNameProvider.java
+++ src/main/java/org/mule/extension/langchain/internal/llm/LangchainLLMParameterModelNameProvider.java
@@ -1,8 +1,5 @@
 package org.mule.extension.langchain.internal.llm;
-
 import java.util.Set;
-
-
 import org.mule.runtime.api.value.Value;
 import org.mule.runtime.extension.api.values.ValueBuilder;
 import org.mule.runtime.extension.api.values.ValueProvider;
@@ -10,16 +7,34 @@ import org.mule.runtime.extension.api.values.ValueResolvingException;
 
 public class LangchainLLMParameterModelNameProvider implements ValueProvider {
 
+	private static final Set<Value> VALUES_FOR = ValueBuilder.getValuesFor(
+	"gpt-3.5-turbo",
+	"gpt-4",
+	"gpt-4-turbo",
+	"dall-e-3",
+	"mistral-small-latest",
+	"mistral-medium-latest",
+	"mistral-large-latest",
+	"mistral",
+	"phi3",
+	"orca-mini",
+	"llama2",
+	"codellama",
+	"tinyllama",
+	"claude-3-haiku-20240307",
+	"claude-3-opus-20240229",
+	"claude-3-sonnet-20240229",
+	"anthropic.claude-instant-v1",
+	"anthropic.claude-v2",
+	"anthropic.claude-v2:1",
+	"anthropic.claude-3-sonnet-20240229-v1:0",
+	"anthropic.claude-3-haiku-20240307-v1:0");
+
 	@Override
 	public Set<Value> resolve() throws ValueResolvingException {
-		// TODO Auto-generated method stub
 		
-		return ValueBuilder.getValuesFor("gpt-3.5-turbo","gpt-4","gpt-4-turbo","dall-e-3",
-										"mistral-small-latest","mistral-medium-latest", "mistral-large-latest", 
-										"mistral","phi3","orca-mini","llama2","codellama","tinyllama",
-										"claude-3-haiku-20240307","claude-3-opus-20240229","claude-3-sonnet-20240229",
-										"anthropic.claude-instant-v1", "anthropic.claude-v2", "anthropic.claude-v2:1",
-										"anthropic.claude-3-sonnet-20240229-v1:0","anthropic.claude-3-haiku-20240307-v1:0");
+		
+		return VALUES_FOR;
 	}
 
 }
