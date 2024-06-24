a75b9996aed9abb40cfde91a4915047e5d94c169
Amir Khan <amir.khan@salesforce.com>
2024-06-24 19:44:56 +0200

    java17

--- src/main/java/org/mule/extension/mulechain/internal/LangchainExtension.java
+++ src/main/java/org/mule/extension/mulechain/internal/LangchainExtension.java
@@ -6,7 +6,10 @@ import org.mule.extension.mulechain.internal.llm.LangchainLLMConfiguration;
 import org.mule.extension.mulechain.internal.tools.LangchainToolsConfiguration;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
-
+import org.mule.sdk.api.annotation.JavaVersionSupport;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_11;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_17;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
 
 /**
  * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
@@ -15,6 +18,7 @@ import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 @Xml(prefix = "mulechain")
 @Extension(name = "MuleChain AI")
 @Configurations({LangchainLLMConfiguration.class})
+@JavaVersionSupport({JAVA_8, JAVA_11, JAVA_17})
 public class LangchainExtension {
 
 }
