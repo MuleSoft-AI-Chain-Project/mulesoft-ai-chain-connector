e972e1741e56c99cdc50f00fb6dc9dad0cf81f7f
ARPIT GUPTA <gupta.arpit@salesforce.com>
2024-08-01 18:16:07 +0530

    W-16354625: Removed static map from value provider

--- src/main/java/org/mule/extension/mulechain/internal/extension/MuleChainConnector.java
+++ src/main/java/org/mule/extension/mulechain/internal/extension/MuleChainConnector.java
@@ -1,10 +1,14 @@
 package org.mule.extension.mulechain.internal.extension;
 
+import org.mule.runtime.api.meta.Category;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.extension.mulechain.internal.config.LangchainLLMConfiguration;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.runtime.extension.api.annotation.license.RequiresEnterpriseLicense;
 import org.mule.sdk.api.annotation.JavaVersionSupport;
+
+
 import static org.mule.sdk.api.meta.JavaVersion.JAVA_11;
 import static org.mule.sdk.api.meta.JavaVersion.JAVA_17;
 import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
@@ -14,9 +18,10 @@ import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
  * and sources are going to be declared.
  */
 @Xml(prefix = "mulechain")
-@Extension(name = "MuleChain AI")
+@Extension(name = "MuleChain AI", category = Category.CERTIFIED)
 @Configurations({LangchainLLMConfiguration.class})
+@RequiresEnterpriseLicense(allowEvaluationLicense = true)
 @JavaVersionSupport({JAVA_8, JAVA_11, JAVA_17})
-public class LangchainExtension {
+public class MuleChainConnector {
 
 }
--- src/main/java/org/mule/extension/mulechain/internal/llm/LangchainLLMModelNameProvider.java
+++ src/main/java/org/mule/extension/mulechain/internal/llm/LangchainLLMModelNameProvider.java
@@ -1,15 +1,7 @@
 package org.mule.extension.mulechain.internal.llm;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
-import dev.langchain4j.model.anthropic.AnthropicChatModelName;
-import dev.langchain4j.model.mistralai.MistralAiChatModelName;
-import dev.langchain4j.model.openai.OpenAiChatModelName;
-import dev.langchain4j.model.openai.OpenAiImageModelName;
 import org.mule.extension.mulechain.internal.llm.type.LangchainLLMType;
 import org.mule.runtime.api.value.Value;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
@@ -19,27 +11,12 @@ import org.mule.runtime.extension.api.values.ValueResolvingException;
 
 public class LangchainLLMModelNameProvider implements ValueProvider {
 
-  private static final Map<LangchainLLMType, Stream<String>> valueMap;
-
-  static {
-    valueMap = new HashMap<>();
-    Stream<String> openAiStream = Stream
-        .concat(Arrays.stream(OpenAiChatModelName.values()), Arrays.stream(OpenAiImageModelName.values())).map(String::valueOf);
-    valueMap.put(LangchainLLMType.OPENAI, openAiStream);
-    valueMap.put(LangchainLLMType.GROQAI_OPENAI, openAiStream);
-    valueMap.put(LangchainLLMType.MISTRAL_AI, Arrays.stream(MistralAiChatModelName.values()).map(String::valueOf));
-    valueMap.put(LangchainLLMType.OLLAMA,
-                 Arrays.stream(new String[] {"mistral", "phi3", "orca-mini", "llama2", "codellama", "tinyllama"}));
-    valueMap.put(LangchainLLMType.ANTHROPIC, Arrays.stream(AnthropicChatModelName.values()).map(String::valueOf));
-    valueMap.put(LangchainLLMType.AZURE_OPENAI, openAiStream);
-  }
-
   @Parameter
   private String llmType;
 
   @Override
   public Set<Value> resolve() throws ValueResolvingException {
-    return ValueBuilder.getValuesFor(valueMap.get(LangchainLLMType.valueOf(llmType)));
+    return ValueBuilder.getValuesFor(LangchainLLMType.valueOf(llmType).getModelNameStream());
   }
 
 }
