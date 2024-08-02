91ee837471c9afc268aac34491580fc6b633b839
ARPIT GUPTA <gupta.arpit@salesforce.com>
2024-08-02 15:41:36 +0530

    W-16354625: Reformatted code

--- src/main/java/org/mule/extension/mulechain/internal/error/MuleChainErrorType.java
+++ src/main/java/org/mule/extension/mulechain/internal/error/MuleChainErrorType.java
@@ -3,9 +3,6 @@ package org.mule.extension.mulechain.internal.error;
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
 
 public enum MuleChainErrorType implements ErrorTypeDefinition<MuleChainErrorType> {
-    INVALID_AUTHENTICATION,
-    IO_EXCEPTION,
-    TIME_OUT,
-    RATE_LIMIT_OR_QUOTA_EXCEEDED;
+  INVALID_AUTHENTICATION, IO_EXCEPTION, TIME_OUT, RATE_LIMIT_OR_QUOTA_EXCEEDED;
 
 }
