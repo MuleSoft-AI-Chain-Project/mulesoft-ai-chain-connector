412e063f7c9a630560bfa45c20848f04199d4f5f
ARPIT GUPTA <gupta.arpit@salesforce.com>
2024-08-12 16:48:56 +0530

    W-16295334: Removed static initialisations from the code

--- src/main/java/org/mule/extension/mulechain/internal/config/LangchainLLMConfiguration.java
+++ src/main/java/org/mule/extension/mulechain/internal/config/LangchainLLMConfiguration.java
@@ -4,19 +4,15 @@
 package org.mule.extension.mulechain.internal.config;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import org.mule.extension.mulechain.internal.exception.config.ConfigValidationException;
 import org.mule.extension.mulechain.internal.operation.LangchainEmbeddingStoresOperations;
 import org.mule.extension.mulechain.internal.operation.LangchainImageModelsOperations;
 import org.mule.extension.mulechain.internal.llm.type.LangchainLLMType;
 import org.mule.extension.mulechain.internal.llm.ConfigTypeProvider;
-import org.mule.extension.mulechain.internal.config.util.LangchainLLMInitializerUtil;
 import org.mule.extension.mulechain.internal.operation.LangchainLLMOperations;
 import org.mule.extension.mulechain.internal.llm.LangchainLLMModelNameProvider;
 import org.mule.extension.mulechain.internal.llm.LangchainLLMTypeProvider;
 import org.mule.extension.mulechain.internal.llm.config.ConfigExtractor;
 import org.mule.extension.mulechain.internal.llm.config.ConfigType;
-import org.mule.extension.mulechain.internal.llm.config.EnvConfigExtractor;
-import org.mule.extension.mulechain.internal.llm.config.FileConfigExtractor;
 import org.mule.runtime.api.lifecycle.Initialisable;
 import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.api.meta.ExpressionSupport;
@@ -30,11 +26,7 @@ import org.mule.runtime.extension.api.annotation.param.display.Placement;
 import org.mule.runtime.extension.api.annotation.param.display.Summary;
 import org.mule.runtime.extension.api.annotation.values.OfValues;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 /**
  * This class represents an extension configuration, values set in this class are commonly used across multiple
@@ -43,23 +35,6 @@ import java.util.function.Function;
 @Configuration(name = "llm-configuration")
 @Operations({LangchainLLMOperations.class, LangchainEmbeddingStoresOperations.class, LangchainImageModelsOperations.class})
 public class LangchainLLMConfiguration implements Initialisable {
-
-  private static final Map<LangchainLLMType, BiFunction<ConfigExtractor, LangchainLLMConfiguration, ChatLanguageModel>> llmMap;
-  private static final Map<ConfigType, Function<LangchainLLMConfiguration, ConfigExtractor>> configExtractorMap;
-
-  static {
-    configExtractorMap = new HashMap<>();
-    configExtractorMap.put(ConfigType.ENV_VARIABLE, (configuration) -> new EnvConfigExtractor());
-    configExtractorMap.put(ConfigType.CONFIG_JSON, FileConfigExtractor::new);
-
-    llmMap = new HashMap<>();
-    llmMap.put(LangchainLLMType.OPENAI, (LangchainLLMInitializerUtil::createOpenAiChatModel));
-    llmMap.put(LangchainLLMType.GROQAI_OPENAI, (LangchainLLMInitializerUtil::createGroqOpenAiChatModel));
-    llmMap.put(LangchainLLMType.MISTRAL_AI, (LangchainLLMInitializerUtil::createMistralAiChatModel));
-    llmMap.put(LangchainLLMType.OLLAMA, (LangchainLLMInitializerUtil::createOllamaChatModel));
-    llmMap.put(LangchainLLMType.ANTHROPIC, (LangchainLLMInitializerUtil::createAnthropicChatModel));
-    llmMap.put(LangchainLLMType.AZURE_OPENAI, (LangchainLLMInitializerUtil::createAzureOpenAiChatModel));
-  }
 
   @Parameter
   @Placement(order = 1, tab = Placement.DEFAULT_TAB)
@@ -80,29 +55,29 @@ public class LangchainLLMConfiguration implements Initialisable {
   @Parameter
   @Expression(ExpressionSupport.SUPPORTED)
   @OfValues(LangchainLLMModelNameProvider.class)
-  @Placement(order = 4)
+  @Placement(order = 4, tab = Placement.DEFAULT_TAB)
   private String modelName;
 
   @Parameter
-  @Placement(order = 5)
+  @Placement(order = 5, tab = Placement.DEFAULT_TAB)
   @Optional(defaultValue = "0.7")
   private double temperature = 0.7;
 
   @Parameter
-  @Placement(order = 6)
+  @Placement(order = 6, tab = Placement.DEFAULT_TAB)
   @Optional(defaultValue = "60")
   @DisplayName("LLM timeout")
   private int llmTimeout = 60;
 
   @Parameter
   @Optional(defaultValue = "SECONDS")
-  @Placement(order = 7)
+  @Placement(order = 7, tab = Placement.DEFAULT_TAB)
   @DisplayName("LLM timeout unit")
   @Summary("Time unit to be used in the LLM Timeout")
   private TimeUnit llmTimeoutUnit = TimeUnit.SECONDS;
 
   @Parameter
-  @Placement(order = 8)
+  @Placement(order = 8, tab = Placement.DEFAULT_TAB)
   @Expression(ExpressionSupport.SUPPORTED)
   @Optional(defaultValue = "500")
   private int maxTokens = 500;
@@ -152,21 +127,14 @@ public class LangchainLLMConfiguration implements Initialisable {
   }
 
   private ChatLanguageModel createModel(ConfigExtractor configExtractor) {
-    LangchainLLMType type = LangchainLLMType.valueOf(llmType);
-    if (llmMap.containsKey(type)) {
-      return llmMap.get(type).apply(configExtractor, this);
-    }
-    throw new ConfigValidationException("LLM Type not supported: " + llmType);
+    LangchainLLMType type = LangchainLLMType.fromValue(llmType);
+    return type.getConfigBiFunction().apply(configExtractor, this);
   }
 
   @Override
   public void initialise() throws InitialisationException {
     ConfigType config = ConfigType.fromValue(configType);
-    if (configExtractorMap.containsKey(config)) {
-      configExtractor = configExtractorMap.get(config).apply(this);
-      model = createModel(configExtractor);
-    } else {
-      throw new ConfigValidationException("Config Type not supported: " + configType);
-    }
+    configExtractor = config.getConfigExtractorFunction().apply(this);
+    model = createModel(configExtractor);
   }
 }
