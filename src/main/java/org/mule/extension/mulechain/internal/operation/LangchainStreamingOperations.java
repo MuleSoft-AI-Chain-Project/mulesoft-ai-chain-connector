8d7b61bd9edb6b7d34df5e019f41cc145bdb1018
ARPIT GUPTA <gupta.arpit@salesforce.com>
2024-08-13 10:40:37 +0530

    W-16295334: Added one streaming operation for chat

---
+++ src/main/java/org/mule/extension/mulechain/internal/operation/LangchainStreamingOperations.java
@@ -0,0 +1,87 @@
+/**
+ * (c) 2003-2024 MuleSoft, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1 a copy of which has been included with this distribution in the LICENSE.md file.
+ */
+package org.mule.extension.mulechain.internal.operation;
+
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.TokenStream;
+import org.mule.extension.mulechain.internal.config.LangchainLLMConfiguration;
+import org.mule.extension.mulechain.internal.error.provider.AiServiceErrorTypeProvider;
+import org.mule.extension.mulechain.internal.exception.ChatException;
+import org.mule.extension.mulechain.internal.exception.StreamingException;
+import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.error.Throws;
+import org.mule.runtime.extension.api.annotation.param.Config;
+import org.mule.runtime.extension.api.annotation.param.MediaType;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.nio.charset.StandardCharsets;
+
+import static java.time.Duration.ofSeconds;
+import static org.mule.runtime.extension.api.annotation.param.MediaType.ANY;
+
+/**
+ * This class is a container for operations, every public method in this class will be taken as an extension operation.
+ */
+public class LangchainStreamingOperations {
+
+  interface Assistant {
+
+    TokenStream chat(String userMessage);
+  }
+
+  /**
+   * Implements a simple Chat agent
+   */
+  @MediaType(value = ANY, strict = false)
+  @Alias("CHAT-answer-prompt-w-stream")
+  @Throws(AiServiceErrorTypeProvider.class)
+  public InputStream answerPromptByModelNameStream(@Config LangchainLLMConfiguration configuration, String prompt) {
+    String openaiApiKey = configuration.getConfigExtractor().extractValue("OPENAI_API_KEY");
+    long durationInSec = configuration.getLlmTimeoutUnit().toSeconds(configuration.getLlmTimeout());
+    try {
+      StreamingChatLanguageModel model = OpenAiStreamingChatModel.builder()
+          .apiKey(openaiApiKey)
+          .modelName(configuration.getModelName())
+          .maxTokens(configuration.getMaxTokens())
+          .temperature(configuration.getTemperature())
+          .timeout(ofSeconds(durationInSec))
+          .build();
+      Assistant assistant = AiServices.create(Assistant.class, model);
+      TokenStream tokenStream = assistant.chat(prompt);
+
+      PipedOutputStream pipedOutputStream = new PipedOutputStream();
+      PipedInputStream pipedInputStream = new PipedInputStream(pipedOutputStream);
+
+      tokenStream.onNext(value -> {
+        try {
+          pipedOutputStream.write(value.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+          throw new StreamingException("Error occurred while streaming output", e);
+        }
+      })
+          .onComplete(response -> {
+            try {
+              pipedOutputStream.close();
+            } catch (IOException e) {
+              throw new StreamingException("Error occurred while closing the stream", e);
+            }
+          })
+          .onError(throwable -> {
+            throw new StreamingException("Exception occurred onError()", throwable);
+          })
+          .start();
+      return pipedInputStream;
+    } catch (Exception e) {
+      throw new ChatException("Unable to respond with the chat provided", e);
+    }
+  }
+
+
+
+}
