fa2485b066bd702cbd2992ba79ecbb574e7a738f
dipeshkumardutta-sfemu <160039229+dipeshkumardutta-sfemu@users.noreply.github.com>
2024-10-28 15:24:44 +0530

    Update LangchainLLMType.java

--- src/main/java/org/mule/extension/mulechain/internal/llm/type/LangchainLLMType.java
+++ src/main/java/org/mule/extension/mulechain/internal/llm/type/LangchainLLMType.java
@@ -96,7 +96,7 @@ public enum LangchainLLMType {
   }
 
   enum HuggingFaceModelName {
-    TII_UAE_FALCON_7B_INSTRUCT("tiiuae/falcon-7b-instruct"), PHI3("microsoft/Phi-3.5-mini-instruct"), MISTRAL_7B_INSTRUCT_v03(
+    TII_UAE_FALCON_7B_INSTRUCT("tiiuae/falcon-7b-instruct"), PHI3("microsoft/Phi-3.5-mini-instruct"), MISTRAL_7B_INSTRUCT_V03(
         "mistralai/Mistral-7B-Instruct-v0.3"), TINY_LLAMA("TinyLlama/TinyLlama-1.1B-Chat-v1.0");
 
     private final String value;
